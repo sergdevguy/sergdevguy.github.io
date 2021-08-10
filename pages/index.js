aaee157ecc809d7c7fe7c00df82cb8a90a38f223
Sergdevguy <sergdevguy@gmail.com>
2021-08-11 01:46:42 +0300

    Add next.js starting template.

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Приветс</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <h1>Привет</h1>
+        <div>
+          <div>
+            <Link href="/posts/first-post">
+              <a>Что-нибудь</a>
+            </Link>
+          </div>
+          <div>
+            <Link href="/games/games">
+              <a>Игрули</a>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
