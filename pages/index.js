9ed17f47cc0e7b84cb92a9387938d6068a97145c
Sergdevguy <sergdevguy@gmail.com>
2021-08-12 04:21:59 +0300

    In progress width content and styles.

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,14 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Приветс</title>
-      </Head>
+    <Layout>
       <section className={utilStyles.headingMd}>
-        <h1>Привет</h1>
+        <h1>Привет.<br/> Здесь всякое можно потыкать, посмотреть</h1>
         <div>
-          <div>
-            <Link href="/somethingElse">
-              <a>Что-нибудь</a>
-            </Link>
-          </div>
           <div>
             <Link href="/games/games">
               <a>Игрули</a>
@@ -23,6 +16,6 @@ export default function Home() {
           </div>
         </div>
       </section>
-    </>
+    </Layout>
   )
 }
