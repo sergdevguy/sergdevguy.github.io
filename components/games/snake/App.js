54c35106d7942454a564b0c1c8f3b42322f8c300
Sergdevguy <sergdevguy@gmail.com>
2021-10-16 21:45:04 +0300

    Fix snake game move interval

--- components/games/snake/App.js
+++ components/games/snake/App.js
@@ -181,7 +181,7 @@ function App() {
     const timer = setInterval(() => {
       dispatch({ type: 'moveSnake' });
       dispatch({ type: 'drawField' });
-    }, 6500);
+    }, 500);
     return () => clearInterval(timer);
   }, [state.win]);
 
