9a21aa7392ad1ca4899bc8c0ce30b848b22bf9d6
Sergdevguy <sergdevguy@gmail.com>
2021-11-05 09:01:19 +0300

    Test adaptive styles in nav

--- components/nav/Nav.js
+++ components/nav/Nav.js
@@ -5,36 +5,38 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemButton from '@material-ui/core/ListItemButton';
 import ListItemText from '@material-ui/core/ListItemText';
+// styles
+import s from './Nav.module.scss';
 
 export default function Nav() {
     return (
-        <nav>
-            <List style={{ display: 'flex' }}>
+        <nav className="nav">
+            <List style={{ display: 'flex', flexWrap: 'wrap' }}>
                 <ListItem disablePadding style={{ width: 'auto' }}>
                     <Link href="/" passHref>
                         <ListItemButton>
-                            <ListItemText align="center">Главная</ListItemText>
+                            <ListItemText align="center" className={s['nav__item']}>Главная</ListItemText>
                         </ListItemButton>
                     </Link>
                 </ListItem>
                 <ListItem disablePadding style={{ width: 'auto' }}>
                     <Link href="/todo" passHref>
                         <ListItemButton>
-                            <ListItemText align="center">Задачник</ListItemText>
+                            <ListItemText align="center" className={s['nav__item']}>Задачник</ListItemText>
                         </ListItemButton>
                     </Link>
                 </ListItem>
                 <ListItem disablePadding style={{ width: 'auto' }}>
                     <Link href="/games" passHref>
                         <ListItemButton>
-                            <ListItemText align="center">Игрули</ListItemText>
+                            <ListItemText align="center" className={s['nav__item']}>Игрули</ListItemText>
                         </ListItemButton>
                     </Link>
                 </ListItem>
                 <ListItem disablePadding style={{ width: 'auto' }}>
                     <Link href="/programs" passHref>
                         <ListItemButton>
-                            <ListItemText align="center">Программы</ListItemText>
+                            <ListItemText align="center" className={s['nav__item']}>Программы</ListItemText>
                         </ListItemButton>
                     </Link>
                 </ListItem>
