1c60adf72bbc2efbe89b74db49c5d90aac4205cd
Sergdevguy <sergdevguy@gmail.com>
2024-06-16 08:13:09 +0300

    fix styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,9 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
