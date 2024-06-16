3dd239ece076484d30ba480d6044e4fa624d39dd
Sergdevguy <sergdevguy@gmail.com>
2024-06-16 07:46:06 +0300

    build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
     basePath: process.env.NODE_ENV === 'production' ? '' : '',
 };
