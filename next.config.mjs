9ce8d12fc720f8bd44ed9b1e49474a813de7d726
Sergdevguy <sergdevguy@gmail.com>
2024-06-16 07:31:48 +0300

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '' : '',
+};
 
 export default nextConfig;
