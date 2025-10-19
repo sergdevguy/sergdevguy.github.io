85cb7f1ee923716b744fb4bdcdc7bf9332f4c403
Sergey Harchenko <sergdevguy@gmail.com>
2025-10-19 14:56:56 +0300

    init

---
+++ pages/games/easy/[slug].vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="game-frame">
+    <iframe :src="`/games/easy/${$route.params.slug}/index.html`" frameborder="0"></iframe>
+  </div>
+</template>
+
+<style scoped>
+  .game-frame {
+    width: 100vw;
+    height: 100vh;
+  }
+
+  iframe {
+    width: 100%;
+    height: 100%;
+    border: none;
+  }
+</style>
